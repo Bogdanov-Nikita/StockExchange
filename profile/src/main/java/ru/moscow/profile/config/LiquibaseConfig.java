df63548c8d2e3cbc4ff4260406856acaccb76595
Nbogdanov <NBogdanov@it-one.ru>
2023-12-09 00:46:28 +0300

    add liquidBase config

---
+++ profile/src/main/java/ru/moscow/profile/config/LiquibaseConfig.java
@@ -0,0 +1,22 @@
+package ru.moscow.profile.config;
+
+import liquibase.integration.spring.SpringLiquibase;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Slf4j
+@Configuration
+public class LiquibaseConfig {
+
+    @Bean
+    public SpringLiquibase liquibase(DataSource dataSource) {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setChangeLog("classpath:database/changesets/changelog.xml");
+        liquibase.setDataSource(dataSource);
+        return liquibase;
+    }
+
+}
