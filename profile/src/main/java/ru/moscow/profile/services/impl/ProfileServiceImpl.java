37b36f83b58ccb3d792095e5c74262d1b09cb707
Nbogdanov <NBogdanov@it-one.ru>
2023-12-08 14:00:03 +0300

    add service

--- profile/src/main/java/ru/moscow/profile/services/impl/ProfileServiceImpl.java
+++ profile/src/main/java/ru/moscow/profile/services/impl/ProfileServiceImpl.java
@@ -1,12 +1,10 @@
 package ru.moscow.profile.services.impl;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.validation.Validator;
 import org.springframework.web.server.ResponseStatusException;
 import ru.moscow.profile.dto.ProfileResponse;
 import ru.moscow.profile.dto.ProfileSearchRequest;
