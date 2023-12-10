2abb400ff638abb730e37e06b0afb302adf142fd
Nbogdanov <NBogdanov@it-one.ru>
2023-12-10 03:23:22 +0300

    supports move to default in interface ApplicationValidator

--- profile/src/main/java/ru/moscow/profile/validators/ApplicationValidator.java
+++ profile/src/main/java/ru/moscow/profile/validators/ApplicationValidator.java
@@ -2,6 +2,7 @@ package ru.moscow.profile.validators;
 
 import org.springframework.validation.Validator;
 import ru.moscow.profile.dto.ApplicationType;
+import ru.moscow.profile.dto.ProfileCreateRequest;
 
 /**
  * Обобщение для проверок
@@ -9,6 +10,11 @@ import ru.moscow.profile.dto.ApplicationType;
  * учётной записи из разных приложений для каждого приложения своя реализация
  */
 public interface ApplicationValidator extends Validator {
+
+    @Override
+    default boolean supports(Class<?> clazz) {
+        return ProfileCreateRequest.class.equals(clazz);
+    }
 
     /**
      * @return тип приложения
